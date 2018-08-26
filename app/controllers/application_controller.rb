ec2ea21e93308e1235a3ed267e41449c1373dce8
Alfonso <ajzuniga1@miuandes.cl>
2018-08-26 17:40:12 -0300

    Advanced in homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def index
+    @now_playing = Tmdb::Movie.now_playing['results']
+    @upcomming = Tmdb::Movie.upcoming['results']
+    @popular = Tmdb::Movie.popular['results']
+    @top_rated = Tmdb::Movie.top_rated['results']
+    @movie = Tmdb::Search.movie('The Meg', page: 1)['results']
+  end
 end
